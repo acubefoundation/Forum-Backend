3d914a5a8dd1e71d165f173892f97dcec98d1ac7
acubefoundation <abdurahimamin987@gamil.com>
2023-08-02 11:37:15 +0300

    first commit

---
+++ server/config/database.js
@@ -0,0 +1,99 @@
+//database.js
+    const mysql = require('mysql2')
+
+const pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    database: process.env.DB,
+    password: process.env.PASS,
+    connectionLimit: 10,
+
+  });
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+    } else {
+      console.log('Connected to MySQL successfully');
+      connection.release(); // Release the connection back to the pool
+    }
+  });
+  
+
+
+
+
+let registration =  `CREATE TABLE if not exists registration(
+   
+    user_id int auto_increment,
+    user_name varchar(255) not null,
+    user_email varchar(255) not null,
+    user_password varchar(255) not null,
+    PRIMARY KEY (user_id)
+)`; 
+
+let profile =  `CREATE TABLE if not exists profile(
+    user_profile_id int auto_increment,
+    user_id int not null,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    PRIMARY KEY (user_profile_id),
+    FOREIGN KEY (user_id) REFERENCES registration(user_id) 
+)`; 
+
+
+let question =  `CREATE TABLE if not exists question(
+    question_id int auto_increment,
+    question varchar(255) not null,
+    question_description varchar(255),
+    question_code_block varchar(255),
+    tags varchar(255),
+    post_id varchar(255) not null,
+    user_id int not null,
+    PRIMARY KEY (question_id),
+    UNIQUE KEY (post_id),
+    FOREIGN KEY (user_id) REFERENCES registration(user_id)
+)`; 
+let answer =  `CREATE TABLE if not exists answer(
+    answer_id int auto_increment,
+    answer varchar(255) not null,
+    answer_code_block varchar(255),
+    user_id int not null,
+    question_id int not null,
+    PRIMARY KEY (answer_id),
+    FOREIGN KEY (user_id)  REFERENCES registration(user_id),
+    FOREIGN KEY (question_id)  REFERENCES registration(user_id)
+)`; 
+
+
+pool.query(registration, (err, results, fields) => {
+    if (err) {console.log(err)}else {
+        console.log("table registration created")
+    }
+});
+
+
+
+pool.query(profile, (err, results, fields) => {
+    if (err) {console.log(err)}else {
+        console.log("table profile created")
+    }
+});
+
+
+pool.query(question, (err, results, fields) => {
+    if (err) {console.log(err)}else {
+        console.log("table question created")
+    }
+});
+
+
+pool.query(answer, (err, results, fields) => {
+    if (err) {console.log(err)}else {
+        console.log("table answer created")
+    }
+});
+
+
+
+module.exports = pool;
