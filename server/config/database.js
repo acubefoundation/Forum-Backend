91dd4ddea25adf440708ad66c7336cbf6aebeb97
acubefoundation <abdurahimamin987@gamil.com>
2023-08-03 17:15:25 +0300

    -

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,39 @@
 //database.js
-    const mysql = require('mysql2')
+        const mysql = require('mysql2');
+require('dotenv').config()
+
+    // const pool = mysql.createPool({
+    //   connectionLimit: 10,
+    //   host: 'aws.connect.psdb.cloud',
+    //   user: '2hc9eqsqrm3n65k79l2b',
+    //   password: 'pscale_pw_4PktMuQwfBdzTC07eq3Tq8lIBohdqA4qhVeeDf6yPXO',
+    //   database: 'evangadi-forum',
+    //   debug: true,
+    //   acquireTimeout: 30000
+
+    // //   ssl: {
+    // //     rejectUnauthorized: true
+    // //   }
+    // });
+
+// const pool = mysql.createConnection(process.env.DATABASE_URL)
+
+    // const retryConnection = () => {
+    //     console.log('Retrying database connection...');
+    //     pool.getConnection((err, connection) => {
+    //       if (err) {
+    //         console.error('Error connecting to MySQL:', err);
+    //         setTimeout(retryConnection, 5000); // Retry after 5 seconds
+    //       } else {
+    //         console.log('Connected to MySQL successfully');
+    //         connection.release();
+    //       }
+    //     });
+    //   };
+      
+    //   retryConnection();
+      
+    
 
 const pool = mysql.createPool({
     host: process.env.HOST,
@@ -10,6 +44,7 @@ const pool = mysql.createPool({
 
   });
 
+
   pool.getConnection((err, connection) => {
     if (err) {
       console.error('Error connecting to MySQL:', err);
@@ -18,31 +53,24 @@ const pool = mysql.createPool({
       connection.release(); // Release the connection back to the pool
     }
   });
-  
 
-
-
-
-let registration =  `CREATE TABLE if not exists registration(
-   
+let registration = `CREATE TABLE if not exists registration (
     user_id int auto_increment,
     user_name varchar(255) not null,
     user_email varchar(255) not null,
     user_password varchar(255) not null,
     PRIMARY KEY (user_id)
-)`; 
+)`;
 
-let profile =  `CREATE TABLE if not exists profile(
+let profile = `CREATE TABLE if not exists profile (
     user_profile_id int auto_increment,
     user_id int not null,
     first_name varchar(255) not null,
     last_name varchar(255) not null,
-    PRIMARY KEY (user_profile_id),
-    FOREIGN KEY (user_id) REFERENCES registration(user_id) 
-)`; 
+    PRIMARY KEY (user_profile_id)
+)`;
 
-
-let question =  `CREATE TABLE if not exists question(
+let question = `CREATE TABLE if not exists question (
     question_id int auto_increment,
     question varchar(255) not null,
     question_description varchar(255),
@@ -51,32 +79,31 @@ let question =  `CREATE TABLE if not exists question(
     post_id varchar(255) not null,
     user_id int not null,
     PRIMARY KEY (question_id),
-    UNIQUE KEY (post_id),
-    FOREIGN KEY (user_id) REFERENCES registration(user_id)
-)`; 
-let answer =  `CREATE TABLE if not exists answer(
+    UNIQUE KEY (post_id)
+)`;
+
+let answer = `CREATE TABLE if not exists answer (
     answer_id int auto_increment,
     answer varchar(255) not null,
     answer_code_block varchar(255),
     user_id int not null,
     question_id int not null,
-    PRIMARY KEY (answer_id),
-    FOREIGN KEY (user_id)  REFERENCES registration(user_id),
-    FOREIGN KEY (question_id)  REFERENCES registration(user_id)
-)`; 
-
+    PRIMARY KEY (answer_id)
+)`;
 
 pool.query(registration, (err, results, fields) => {
-    if (err) {console.log(err)}else {
-        console.log("table registration created")
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("table registration created");
     }
 });
 
-
-
 pool.query(profile, (err, results, fields) => {
-    if (err) {console.log(err)}else {
-        console.log("table profile created")
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("table profile created");
     }
 });
 
