057ac02bca543f48afc311f8dc95477bc7421ac9
acubefoundation <abdurahimamin987@gamil.com>
2023-08-03 09:56:21 +0300

    server updated

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api/users", userRouter);
-
+//listen to port 3435
+app
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
